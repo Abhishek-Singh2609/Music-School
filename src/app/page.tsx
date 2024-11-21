6c8e67fcbae9867617e76d608caa2af26cac1a5b
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2024-11-21 19:14:38 +0530

    add footers cards and tooltip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import Footer from "@/components/Footer";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
@@ -8,12 +10,13 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-white">
-    <h1 className="text-3xl font-bold text-center">Hii Abhishek</h1>
     <HeroSection />
     <FeaturedCourses />
     <WhyChooseUs />
     <MusicSchoolTestimonials/>
     <UpcomingWebinars/>
+    <Instructors/>
+    <Footer/>
    </main>
   );
 }
--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -64,7 +64,7 @@ export default function UpcomingWebinars() {
           {
             title: webinar.title,
             description: webinar.description,
-            link: '/'
+            link: webinar.slug
           }
         ))}
         />
